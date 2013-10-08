34829612f48e80de0105c38c5d149dbd08f1a3b7
J. Aaron Eaton <aaron@channeleaton.com>
2013-10-08 11:20:13 -0500

    AGSTAFF-4 Fixed title_save_pre filter

--- agrilife-people.php
+++ agrilife-people.php
@@ -12,7 +12,7 @@
 define( 'PEOPLE_PLUGIN_DIRNAME', 'agrilife-people' );
 define( 'PEOPLE_PLUGIN_DIR_PATH', plugin_dir_path( __FILE__ ) );
 define( 'PEOPLE_PLUGIN_DIR_URL', plugin_dir_url( __FILE__ ) );
-define( 'PEOPLE_META_PREFIX', 'als_' );
+define( 'PEOPLE_META_PREFIX', 'ag-people-' );
 
 // Autoload all classes
 spl_autoload_register( 'AgriLife_People::autoload' );
@@ -74,23 +74,26 @@ class AgriLife_People {
     // Direct to the proper templates
     $alp_templates = new ALP_Templates;
 
-    // add_filter( 'title_save_pre', array( $this, 'save_people_title' ) );
+    add_filter( 'title_save_pre', array( $this, 'save_people_title' ) );
 
     $this->add_image_sizes();
 
   }
 
   /**
-   * Saves the staff title as lastname, firstname
-   * @param  string $staff_title The empty staff title
+   * Saves the person title as lastname, firstname
+   * @param  string $people_title The empty staff title
    * @return string              The correct staff title
    */
   public function save_people_title( $people_title ) {
 
-    if ( $_POST['post_type'] == 'staff' ){
-      $first = $_POST[PEOPLE_META_PREFIX . 'first-name'];
-      $last = $_POST[PEOPLE_META_PREFIX . 'last-name'];
-      $staff_title = $last . ', ' . $first;
+    if ( ! empty( $_POST['post_type'] ) && $_POST['post_type'] == 'people' ){
+      global $post;
+
+      $first = $_POST['fields']['field_52540a7be7804'];
+      $last = $_POST['fields']['field_52540aa741c37'];
+
+      $people_title = sprintf( '%2$s, %1$s', $first, $last );
     }
 
     return $people_title;
