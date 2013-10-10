be9f15d2938b1ff5561b3b1f34ee39598c4e0bb7
J. Aaron Eaton <aaron@channeleaton.com>
2013-10-10 15:53:00 -0500

    COALS-48 Fixed people search box display issue

--- archive-people.php
+++ archive-people.php
@@ -13,7 +13,7 @@ get_header(); ?>
 			<label>
 				<h4>Search People Database</h4>
 			</label>
-			<form role="search" class="searchform" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
+			<form role="search" class="people-searchform" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
 				<input type="text" class="s" name="s" id="s" placeholder="Wilber B. Snodgrass" onfocus="if(this.value==this.defaultValue)this.value='';" onblur="if(this.value=='')this.value=this.defaultValue;"/><br />
 				<input type="hidden" name="post_type" value="people" />
 			</form>
