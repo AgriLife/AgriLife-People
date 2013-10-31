61e0b26672cadf425e90bccd54fe34d67bda5af0
J. Aaron Eaton <aaron@channeleaton.com>
2013-10-31 16:52:29 -0500

    AGSTAFF-15 Fixed search capability

--- archive-people.php
+++ archive-people.php
@@ -9,29 +9,19 @@ get_header(); ?>
 <div id="wrap">
 	<div id="content" role="main">
 		<h1 class="entry-title">People</h1>
-		<div class="people-search-form">
-			<label>
-				<h4>Search People Database</h4>
-			</label>
-			<form role="search" class="people-searchform" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
-				<input type="text" class="s" name="s" id="s" placeholder="Wilber B. Snodgrass" onfocus="if(this.value==this.defaultValue)this.value='';" onblur="if(this.value=='')this.value=this.defaultValue;"/><br />
-				<input type="hidden" name="post_type" value="people" />
-			</form>
-		</div>
+		<?php
+		ALP_Templates::search_form();
 
-			<?php 
+		$people = ALP_Query::get_people();
 
-			$people = ALP_Query::get_people();
+		ob_start();
+		require PEOPLE_PLUGIN_DIR_PATH . '/views/people-list.php';
+		$output = ob_get_contents();
+		ob_clean();
 
-			ob_start();
-			require PEOPLE_PLUGIN_DIR_PATH . '/views/people-list.php';
-			$output = ob_get_contents();
-			ob_clean();
+		echo $output;
 
-			echo $output;
-
-			?>
-
+		?>
 
 	</div><!-- #content -->
 
