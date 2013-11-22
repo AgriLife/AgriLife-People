0a4898e33ac06f2c182c3f2ef41d343e4c200d15
Travis Ward <travis@travsiward.com>
2013-11-22 14:21:40 -0600

    Version Update

--- agrilife-people.php
+++ agrilife-people.php
@@ -3,7 +3,7 @@
  * Plugin Name: AgriLife People
  * Plugin URI: http://github.com/AgriLife/AgriLife-People 
  * Description: Creates a people custom post type.
- * Version: 1.0
+ * Version: 1.1
  * Author: J. Aaron Eaton
  * Author URI: http://channeleaton.com
  * License: GPL2
