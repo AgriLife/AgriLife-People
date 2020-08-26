067ad385e16369981d71ca894d57cfedf1251f2c
Zachary Watkins <watkinza@gmail.com>
2020-08-26 16:22:35 -0500

    Update the template file name and address all PHP coding standards issues

--- archive-people.php
+++ archive-people.php
@@ -1,35 +1,50 @@
 <?php
 /**
  * The Template for displaying all people single posts
+ *
+ * @link       https://github.com/AgriLife/agrilife-people/blob/master/archive-people.php
+ * @since      0.1.0
+ * @package    agrilife-people
  */
-
 
 get_header(); ?>
 
-<div
 <?php
-
+$open = '<div';
 if ( ! function_exists( 'genesis_site_layout' ) ) {
-	echo 'id="wrap"';
+	$open .= ' id="wrap"';
 } else {
-	echo 'class="' . genesis_site_layout() . '-wrap"';
+	$class = genesis_site_layout() . '-wrap';
+	if ( 'agriflex4' === get_option( 'stylesheet' ) ) {
+		$class .= ' grid-x grid-padding-x';
+	}
+	$open .= " class=\"$class\"";
 }
+$open .= '>';
+echo wp_kses_post( $open );
 
 ?>
->
-  <div
 	<?php
-
+	$content_open = '<div id="content" role="main"';
 	if ( function_exists( 'genesis_site_layout' ) ) {
-		echo 'class="content" ';
+		$class = 'content';
+
+		if ( 'agriflex4' === get_option( 'stylesheet' ) ) {
+			$class .= ' cell small-12 medium-auto';
+		}
+
+		$content_open .= " class=\"$class\"";
 	}
 
+	$content_open .= '>';
+
+	echo wp_kses_post( $content_open );
+
 	?>
-  id="content" role="main">
 		<h1 class="entry-title">People</h1>
 		<?php
 
-		require_once PEOPLE_PLUGIN_DIR_PATH . '/ALP/Templates.php';
+		require_once PEOPLE_PLUGIN_DIR_PATH . '/ALP/class-alp-templates.php';
 		ALP_Templates::search_form();
 
 		require_once PEOPLE_PLUGIN_DIR_PATH . '/ALP/Query.php';
@@ -40,14 +55,13 @@ if ( ! function_exists( 'genesis_site_layout' ) ) {
 		$output = ob_get_contents();
 		ob_clean();
 
-		echo $output;
+		echo wp_kses_post( $output );
 
 		?>
 	</div><!-- #content -->
 	<?php
-
-	if ( function_exists( 'genesis_site_layout' ) ) {
-		// Genesis theme
+	if ( function_exists( 'genesis_site_layout' ) && 'content-sidebar' === genesis_site_layout() ) {
+		// Genesis theme.
 		get_sidebar();
 	}
 
@@ -56,7 +70,7 @@ if ( ! function_exists( 'genesis_site_layout' ) ) {
 <?php
 
 if ( ! function_exists( 'genesis_site_layout' ) ) {
-	// Not a genesis theme
+	// Not a genesis theme.
 	get_sidebar();
 }
 
