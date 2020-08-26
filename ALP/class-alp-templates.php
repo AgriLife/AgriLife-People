78b4b15a8fe8287a6e33317837ab75f1afb9062c
Zachary Watkins <watkinza@gmail.com>
2020-08-26 15:31:18 -0500

    Remove old single page template from templates file and correct PHPCS warnings and syntax issues

--- ALP/class-alp-templates.php
+++ ALP/class-alp-templates.php
@@ -1,25 +1,38 @@
 <?php
-
 /**
  * Redirects to correct template files based on query variables.
  * Also provides static methods to pull certain views
+ *
+ * @link       https://github.com/AgriLife/agrilife-people/blob/master/ALP/class-alp-templates.php
+ * @since      1.5.7
+ * @package    agrilife-people
+ * @subpackage agrilife-people/ALP
  */
 
+/**
+ * The templates class
+ *
+ * @since 0.1.0
+ * @return void
+ */
 class ALP_Templates {
 
+	/**
+	 * Construct function.
+	 */
 	public function __construct() {
 
 		add_filter( 'archive_template', array( $this, 'get_archive_template' ) );
 		add_filter( 'search_template', array( $this, 'get_search_template' ) );
-		add_filter( 'single_template', array( $this, 'get_single_template' ) );
 		add_filter( 'taxonomy_template', array( $this, 'get_types_template' ) );
 
 	}
 
 	/**
 	 * Shows the archive template when needed
-	 * @param  string $archive_template The default archive template
-	 * @return string                   The correct archive template
+	 *
+	 * @param  string $archive_template The default archive template.
+	 * @return string                   The correct archive template.
 	 */
 	public function get_archive_template( $archive_template ) {
 
@@ -31,52 +44,37 @@ class ALP_Templates {
 
 		return $archive_template;
 
-	} 
+	}
 
 	/**
 	 * Shows the search template when needed
-	 * @param  string $search_template The default search template
-	 * @return string                  The correct search template
+	 *
+	 * @param  string $search_template The default search template.
+	 * @return string                  The correct search template.
 	 */
 	public function get_search_template( $search_template ) {
 
 		global $post;
 
-		if ( get_query_var( 'post_type' ) == 'people' ) {
+		if ( 'people' === get_query_var( 'post_type' ) ) {
 			$search_template = PEOPLE_PLUGIN_DIR_PATH . 'search-people.php';
 		}
 
 		return $search_template;
 
-	} 
-
-	/**
-	 * Shows the single template when needed
-	 * @param  string $single_template The default single template
-	 * @return string                  The correct single template
-	 */
-	public function get_single_template( $single_template ) {
-
-		global $post;
-
-		if ( get_query_var( 'post_type' ) == 'people' ) {
-			$single_template = PEOPLE_PLUGIN_DIR_PATH . '/single-people.php';
-		}
-
-		return $single_template;
-
-	} 
+	}
 
 	/**
 	 * Shows the taxonomy archive when needed
-	 * @param  string $types_template The default taxonomy archive
-	 * @return string                 The correct taxonomy archive
+	 *
+	 * @param  string $types_template The default taxonomy archive.
+	 * @return string                 The correct taxonomy archive.
 	 */
 	public function get_types_template( $types_template ) {
 
 		global $post;
 
-		if ( get_query_var( 'taxonomy' ) == 'types' ) {
+		if ( 'types' === get_query_var( 'taxonomy' ) ) {
 			$types_template = PEOPLE_PLUGIN_DIR_PATH . '/taxonomy-types.php';
 		}
 
@@ -86,6 +84,7 @@ class ALP_Templates {
 
 	/**
 	 * Allows for easy inclusion of the search bar
+	 *
 	 * @return void
 	 */
 	public static function search_form() {
