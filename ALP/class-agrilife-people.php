5c619f38e58d4fe13b97811f73343ced8c3cf4dc
Zachary Watkins <watkinza@gmail.com>
2020-08-26 15:57:37 -0500

    Format post excerpts

--- ALP/class-agrilife-people.php
+++ ALP/class-agrilife-people.php
@@ -92,6 +92,9 @@ class AgriLife_People {
 		// Sort posts by last name field.
 		add_action( 'pre_get_posts', array( $this, 'alp_pre_get_posts' ) );
 
+		// Format People excerpt output.
+		add_filter( 'get_the_excerpt', array( $this, 'filter_site_search_results' ) );
+
 		// Set the post title as Last, First.
 		add_action( 'save_post', array( $this, 'save_people_title' ) );
 
@@ -369,6 +372,135 @@ class AgriLife_People {
 		}
 
 		return $query;
+
+	}
+
+	/**
+	 * Format search result information
+	 *
+	 * @since 1.6.0
+	 * @param string $excerpt The post excerpt.
+	 * @return string
+	 */
+	public function filter_site_search_results( $excerpt ) {
+
+		$type = get_post_type();
+		if ( 'people' === $type ) {
+			// People post types don't have an excerpt, so it's built here.
+			// Based on single-people.php from AgriLife People plugin.
+			$parts = array();
+
+			$title = get_field( 'ag-people-title' );
+			if ( $title ) {
+				$parts[] = 'Title: ' . $title;
+			}
+
+			$office = get_field( 'ag-people-office-location' );
+			if ( $office ) {
+				$parts[] = 'Office: ' . $office;
+			}
+
+			$email = get_field( 'ag-people-email' );
+			if ( $email ) {
+				$parts[] = 'Email: ' . $email;
+			}
+
+			$phone = get_field( 'ag-people-phone' );
+			if ( $phone ) {
+				$parts[] = 'Phone: ' . $phone;
+			}
+
+			$resume = get_field( 'ag-people-resume' );
+			if ( $resume ) {
+				$parts[] = 'Resume/CV: ' . $resume;
+			}
+
+			$website = get_field( 'ag-people-website' );
+			if ( $website ) {
+				$parts[] = $website;
+			}
+
+			$undergrad_items = array();
+			if ( get_field( 'ag-people-undergrad' ) ) {
+				while ( has_sub_field( 'ag-people-undergrad' ) ) :
+					$undergrad_items[] = get_sub_field( 'ag-people-undergrad-degree' );
+					endwhile;
+			}
+			$undergrad = implode( ',', $undergrad_items );
+			if ( ! empty( $undergrad ) ) {
+				$parts[] = 'Undergraduate Education: ' . $undergrad;
+			}
+
+			$grad_items = array();
+			if ( get_field( 'ag-people-graduate' ) ) {
+				while ( has_sub_field( 'ag-people-graduate' ) ) :
+					$grad_items[] = get_sub_field( 'ag-people-graduate-degree' );
+					endwhile;
+			}
+			$grad = implode( ',', $grad_items );
+			if ( ! empty( $grad ) ) {
+				$parts[] = 'Graduate Education: ' . $grad;
+			}
+
+			$award_items = array();
+			if ( get_field( 'ag-people-awards' ) ) {
+				while ( has_sub_field( 'ag-people-awards' ) ) :
+					$award_items[] = get_sub_field( 'ag-people-award' );
+					endwhile;
+			}
+			$award = implode( ', ', $award_items );
+			if ( ! empty( $award ) ) {
+				$parts[] = 'Awards: ' . $award;
+			}
+
+			$taught_items = array();
+			if ( get_field( 'ag-people-courses' ) ) {
+				while ( has_sub_field( 'ag-people-courses' ) ) :
+					$taught_items[] = get_sub_field( 'ag-people-course' );
+					endwhile;
+			}
+			$taught = implode( ', ', $taught_items );
+			if ( ! empty( $taught ) ) {
+				$parts[] = 'Courses Taught: ' . $taught;
+			}
+
+			$content_items = array();
+			while ( has_sub_field( 'ag-people-content' ) ) :
+				$layout = get_row_layout();
+				switch ( $layout ) {
+					case 'ag-people-content-header':
+						$content_items[] = get_sub_field( 'header' );
+						break;
+					case 'ag-people-content-text':
+						$content_items[] = get_sub_field( 'text' );
+						break;
+					default:
+						break;
+				}
+			endwhile;
+			$content = implode( ', ', $content_items );
+			if ( ! empty( $content ) ) {
+				$parts[] = $content;
+			}
+
+			// Concatenate people data to string.
+			$excerpt = implode( '; ', $parts );
+			// Remove HTML.
+			$excerpt = wp_strip_all_tags( $excerpt );
+			// Combine all people data for excerpt.
+			$excerpt = preg_replace( '/ ;/', ';', $excerpt );
+			// Replace special and excess whitespace characters with normal spaces.
+			$excerpt = str_replace( '&nbsp;', ' ', $excerpt );
+			$excerpt = preg_replace( '/[\s\t\r\n\v]+/', ' ', $excerpt );
+			$excerpt = preg_replace( '/[^\w\d,.;"\')]?\h\s/', ' ', $excerpt );
+			// Restrict word count.
+			$excerpt_length = apply_filters( 'excerpt_length', 50 );
+			$excerpt_more   = apply_filters( 'excerpt_more', ' [&hellip;]' );
+			$excerpt        = wp_trim_words( $excerpt, $excerpt_length, $excerpt_more );
+
+		}
+
+		return $excerpt;
 
 	}
 
